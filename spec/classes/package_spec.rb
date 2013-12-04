4a28fab9d32d739c64b6dbb97db6e166a6aa87fb
Marcus Young <myoung34@my.apsu.edu>
2013-12-04 15:28:23 -0600

    Issue 2: Improve tests

---
+++ spec/classes/package_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe 'marklogic' do
+  let(:params) {{ 
+    :licensee    => 'My Company',
+    :license_key => 'foo-bar',
+    :version     => '6.0-4',
+  }}
+
+  let(:title) { 'marklogic' }
+
+  it { should contain_service('MarkLogic') }
+
+  it { should contain_exec('accept_license') }
+  it { should contain_exec('enter_license') }
+  it { should contain_exec('initialize') }
+  it { should contain_exec('install_security_db') }
+  it { should contain_exec('join_cluster') }
+  it { should contain_exec('manually_restart_service') }
+  it { should contain_exec('restart ML') }
+  it { should contain_exec('sleep') }
+  it { should contain_exec('upgrade_databases') }
+
+  it { should contain_package('MarkLogic') }
+  it { should contain_package('gdb').with_ensure('present') }
+  it { should contain_package('glibc-devel.i686').with_ensure('present') }
+  it { should contain_package('glibc-devel.x86_64').with_ensure('present') }
+  it { should contain_package('redhat-lsb').with_ensure('present') }
+  it { should contain_package('wget').with_ensure('installed') }
+
+  it { should contain_firewall('102 allow marklogic').with(
+    'action' => 'accept',
+    'port'   => [ 8000, 8001, 8002 ],
+    'proto'  => 'tcp'
+  )}
+
+end
